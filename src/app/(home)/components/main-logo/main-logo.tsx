43ac57edb2094abf02e323057c14daa2e51c7251
Shannon Cherry <shannon77777@regovtech.com>
2025-03-13 01:34:43 +0800

    Adjusted the first and second svg images to be animated in reverse

--- src/app/(home)/components/main-logo/main-logo.tsx
+++ src/app/(home)/components/main-logo/main-logo.tsx
@@ -6,20 +6,21 @@ import image1 from "../../../../../public/1.svg";
 import image2 from "../../../../../public/2.svg";
 import image3 from "../../../../../public/3.svg";
 import image4 from "../../../../../public/4.svg";
+import { Facebook, Instagram, Linkedin } from "lucide-react";
 
 // Define different variants for each direction
 const imageOneVariants = {
-    hidden: { opacity: 0, x: 100 },
+    hidden: { opacity: 0, x: -150 },
     visible: { opacity: 1, x: 0 }
 };
 
 const imageTwoVariants = {
-    hidden: { opacity: 0, x: -100 },
+    hidden: { opacity: 0, x: 150 },
     visible: { opacity: 1, x: 0 }
 };
 
 const imageThreeVariants = {
-    hidden: { opacity: 0, y: -100 },
+    hidden: { opacity: 0, y: 0 },
     visible: { opacity: 1, y: 0 }
 };
 
@@ -59,7 +60,7 @@ const NEXRVideo: React.FC = () => {
                     initial="hidden"
                     animate="visible"
                     variants={imageThreeVariants}
-                    transition={{ duration: 0.7, delay: 0.6 }}
+                    transition={{ duration: 0.2, delay: 0.9 }}
                     className={styles.imageThreeWrapper}>
                     <Image className={styles.imageThree} src={image3} alt="image3" />
                 </motion.div>
@@ -68,7 +69,7 @@ const NEXRVideo: React.FC = () => {
                     initial="hidden"
                     animate="visible"
                     variants={imageThreeVariants}
-                    transition={{ duration: 0.7, delay: 0.6 }}
+                    transition={{ duration: 0.2, delay: 0.9 }}
                     className={styles.imageThreeAltWrapper}>
                     <Image className={styles.imageThreeAlt} src={image3} alt="image3alt" />
                 </motion.div>
@@ -81,6 +82,29 @@ const NEXRVideo: React.FC = () => {
                     className={styles.imageFourWrapper}>
                     <Image className={styles.imageFour} src={image4} alt="image4" />
                 </motion.div>
+            </div>
+
+            <div className={styles.socialMediaContainer}>
+                <div className={styles.socialMediaInnerDiv}>
+                    <motion.div
+                        initial={{ y: 200, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{ duration: 0.5, ease: "circOut", delay: 1.2 }}>
+                        <Facebook strokeWidth={0.75} />
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 200, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{ duration: 0.5, ease: "circOut", delay: 1.4 }}>
+                        <Instagram strokeWidth={0.75} />
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 200, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{ duration: 0.5, ease: "circOut", delay: 1.6 }}>
+                        <Linkedin strokeWidth={0.75} />
+                    </motion.div>
+                </div>
             </div>
         </div>
     );
