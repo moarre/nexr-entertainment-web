adcae8c3a1dc3afad01ffd0c8d179d1ee24c8c47
Ariff <ariffazlan16@gmail.com>
2025-03-13 02:56:56 +0800

    svgs masked video

--- src/app/(home)/components/main-logo/main-logo.tsx
+++ src/app/(home)/components/main-logo/main-logo.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import styles from './main-logo.module.scss';
-import Image from 'next/image';
-import image1 from '../../../../../public/1.svg';
-import image2 from '../../../../../public/2.svg';
-import image3 from '../../../../../public/3.svg';
-import image4 from '../../../../../public/4.svg';
+// import Image from 'next/image';
+// import image3 from '../../../../../public/3.svg';
 
-// Define different variants for each direction
 const imageOneVariants = {
   hidden: { opacity: 0, x: 100 },
   visible: { opacity: 1, x: 0 },
@@ -19,7 +15,7 @@ const imageTwoVariants = {
 };
 
 const imageThreeVariants = {
-  hidden: { opacity: 0, y: -100 }, 
+  hidden: { opacity: 0, y: -100 },
   visible: { opacity: 1, y: 0 },
 };
 
@@ -31,7 +27,46 @@ const imageFourVariants = {
 const NEXRVideo: React.FC = () => {
   return (
     <div className={styles.container}>
+      {/* Define SVG masks for each video element */}
+      <svg width="0" height="0">
+        <defs>
+          <mask id="mask1">
+            <image
+              href="/1.svg"
+              width="165"
+              height="230"
+              preserveAspectRatio="xMidYMid slice"
+            />
+          </mask>
+          <mask id="mask2">
+            <image
+              href="/2.svg"
+              width="276"
+              height="236"
+              preserveAspectRatio="xMidYMid slice"
+            />
+          </mask>
+          <mask id="mask3">
+            <image
+              href="/3alt.svg"
+              width="100"
+              height="40"
+              preserveAspectRatio="xMidYMid slice"
+            />
+          </mask>
+          <mask id="mask5">
+            <image
+              href="/4.svg"
+              width="345"
+              height="125"
+              preserveAspectRatio="xMidYMid slice"
+            />
+          </mask>
+        </defs>
+      </svg>
+
       <div className={styles.landingContent}>
+        {/* Masked video for imageOne */}
         <motion.div
           initial="hidden"
           animate="visible"
@@ -39,9 +74,19 @@ const NEXRVideo: React.FC = () => {
           transition={{ duration: 0.7, delay: 0 }}
           className={styles.imageOneWrapper}
         >
-          <Image className={styles.imageOne} src={image1} alt="image1" />
+          <video
+            autoPlay
+            loop
+            muted
+            className={styles.imageOne} // same class as before
+            style={{ mask: 'url(#mask1)', WebkitMask: 'url(#mask1)' }}
+          >
+            <source src="/vr-video-2.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </motion.div>
 
+        {/* Masked video for imageTwo */}
         <motion.div
           initial="hidden"
           animate="visible"
@@ -49,9 +94,18 @@ const NEXRVideo: React.FC = () => {
           transition={{ duration: 0.7, delay: 0.3 }}
           className={styles.imageTwoWrapper}
         >
-          <Image className={styles.imageTwo} src={image2} alt="image2" />
+          <video
+            autoPlay
+            loop
+            muted
+            className={styles.imageTwo}
+            style={{ mask: 'url(#mask2)', WebkitMask: 'url(#mask2)' }}
+          >
+            <source src="/vr-video-2.mp4" type="video/mp4" />
+          </video>
         </motion.div>
 
+        {/* Masked video for imageThree */}
         <motion.div
           initial="hidden"
           animate="visible"
@@ -59,19 +113,18 @@ const NEXRVideo: React.FC = () => {
           transition={{ duration: 0.7, delay: 0.6 }}
           className={styles.imageThreeWrapper}
         >
-          <Image className={styles.imageThree} src={image3} alt="image3" />
+          <video
+            autoPlay
+            loop
+            muted
+            className={styles.imageThree}
+            style={{ mask: 'url(#mask3)', WebkitMask: 'url(#mask3)' }}
+          >
+            <source src="/vr-video-2.mp4" type="video/mp4" />
+          </video>
         </motion.div>
 
-        <motion.div
-          initial="hidden"
-          animate="visible"
-          variants={imageThreeVariants}
-          transition={{ duration: 0.7, delay: 0.6 }}
-          className={styles.imageThreeAltWrapper}
-        >
-          <Image className={styles.imageThreeAlt} src={image3} alt="image3alt" />
-        </motion.div>
-
+        {/* Masked video for imageFour */}
         <motion.div
           initial="hidden"
           animate="visible"
@@ -79,7 +132,15 @@ const NEXRVideo: React.FC = () => {
           transition={{ duration: 0.7, delay: 1.2 }}
           className={styles.imageFourWrapper}
         >
-          <Image className={styles.imageFour} src={image4} alt="image4" />
+          <video
+            autoPlay
+            loop
+            muted
+            className={styles.imageFour}
+            style={{ mask: 'url(#mask5)', WebkitMask: 'url(#mask5)' }}
+          >
+            <source src="/vr-video-2.mp4" type="video/mp4" />
+          </video>
         </motion.div>
       </div>
     </div>
