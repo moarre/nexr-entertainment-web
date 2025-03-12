850a95fe160da95a76851022a23fccae13fa429b
Ariff <ariffazlan16@gmail.com>
2025-03-13 00:12:04 +0800

    Combined different svgs to nexr logo

--- src/app/(home)/components/main-logo/main-logo.tsx
+++ src/app/(home)/components/main-logo/main-logo.tsx
@@ -1,51 +1,21 @@
 import React from "react";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import styles from "./main-logo.module.scss";
+import Image from "next/image";
+import image1 from '../../../../../public/1.svg';
+import image2 from '../../../../../public/2.svg';
+import image3 from '../../../../../public/3.svg';
+import image4 from '../../../../../public/4.svg';
 
 const NEXRVideo: React.FC = () => {
     return (
         <div className={styles.container}>
-            <svg width="0" height="0">
-                <defs>
-                    {/* <clipPath id="textClip" clipPathUnits="userSpaceOnUse">
-                        <path d="M48.7216 0.81818V59H38.0966L12.7841 22.3807H12.358V59H0.0568181V0.81818H10.8523L35.9659 37.4091H36.4773V0.81818H48.7216ZM58.8849 59V0.81818H98.0895V10.9602H71.1861V24.8239H96.0724V34.9659H71.1861V48.858H98.2031V59H58.8849ZM119.403 0.81818L131.136 20.6477H131.591L143.381 0.81818H157.273L139.517 29.9091L157.67 59H143.523L131.591 39.142H131.136L119.205 59H105.114L123.324 29.9091L105.455 0.81818H119.403ZM165.057 59V0.81818H188.011C192.405 0.81818 196.155 1.60417 199.261 3.17614C202.386 4.72917 204.763 6.93561 206.392 9.79545C208.04 12.6364 208.864 15.9792 208.864 19.8239C208.864 23.6875 208.03 27.0114 206.364 29.7955C204.697 32.5606 202.282 34.6818 199.119 36.1591C195.975 37.6364 192.169 38.375 187.699 38.375H172.33V28.4886H185.71C188.059 28.4886 190.009 28.1667 191.562 27.5227C193.116 26.8788 194.271 25.9129 195.028 24.625C195.805 23.3371 196.193 21.7367 196.193 19.8239C196.193 17.892 195.805 16.2633 195.028 14.9375C194.271 13.6117 193.106 12.608 191.534 11.9261C189.981 11.2254 188.021 10.875 185.653 10.875H177.358V59H165.057ZM196.477 32.5227L210.938 59H197.358L183.21 32.5227H196.477Z" />
-                    </clipPath> */}
-                    <mask id="imageMask">
-                        {/* for logo only use nexrsvgimage.svg */}
-                        <image href="/nexrsvgimage2.svg" width="200" height="200" />
-                    </mask>
-                </defs>
-            </svg>
-
             <div className={styles.landingContent}>
-                <motion.div 
-                    className={styles.videoContainer}
-                    initial={{ y: -200, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    transition={{ duration: 1, ease: "easeOut" }}
-                >
-                    <div className={styles.videoWrapper}>
-                        <video
-                            autoPlay
-                            loop
-                            muted
-                            className={styles.backgroundVideo}
-                            style={{ clipPath: "url(#textClip)", WebkitClipPath: "url(#textClip)", mask: "url(#imageMask)", }}
-                        >
-                            <source src="/vr-video-2.mp4" type="video/mp4" />
-                            Your browser does not support the video tag.
-                        </video>
-                    </div>
-                </motion.div>
-
-                <motion.div 
-                    className={styles.textContent}
-                    initial={{ y: 200, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    transition={{ duration: 1, ease: "easeOut" }}
-                >
-                    <div className={styles.newText}>COMING SOON</div>
-                </motion.div>
+                <Image className={styles.imageTwo} src={image2} alt="image2" />
+                <Image className={styles.imageOne} src={image1} alt="image1" />
+                <Image className={styles.imageThree} src={image3} alt="image3" />
+                <Image className={styles.imageThreeAlt} src={image3} alt="image3alt" />
+                <Image className={styles.imageFour} src={image4} alt="image4" />
             </div>
         </div>
     );
