6b0b898ead640db51aa03d82586f845998af4b87
Ariff <ariffazlan16@gmail.com>
2025-03-16 05:21:56 +0800

    Allignment and screen size fix

--- src/app/(home)/components/main-logo/main-logo.tsx
+++ src/app/(home)/components/main-logo/main-logo.tsx
@@ -20,7 +20,7 @@ const imageTwoVariants = {
 };
 
 const imageThreeVariants = {
-    hidden: { opacity: 0, y: 0 },
+    hidden: { opacity: 0, y: -100 },
     visible: { opacity: 1, y: 0 }
 };
 
@@ -37,73 +37,86 @@ const NEXRVideo: React.FC = () => {
                     <source src="/vr-video.mp4" type="video/mp4" />
                 </video>
             </div>
-            <div className={styles.landingContent}>
-                <motion.div
-                    initial="hidden"
-                    animate="visible"
-                    variants={imageOneVariants}
-                    transition={{ duration: 0.7, delay: 0 }}
-                    className={styles.imageOneWrapper}>
-                    <Image className={styles.imageOne} src={image1} alt="image1" />
-                </motion.div>
+            <div className={styles.landingContainer}>
+                <div className={styles.landingContent}>
+                    <motion.div
+                        initial="hidden"
+                        animate="visible"
+                        variants={imageTwoVariants}
+                        transition={{ duration: 0.7, delay: 0.3 }}
+                        className={styles.imageTwoWrapper}>
+                        <Image className={styles.imageTwo} src={image2} alt="image2" />
+                    </motion.div>
 
-                <motion.div
-                    initial="hidden"
-                    animate="visible"
-                    variants={imageTwoVariants}
-                    transition={{ duration: 0.7, delay: 0.3 }}
-                    className={styles.imageTwoWrapper}>
-                    <Image className={styles.imageTwo} src={image2} alt="image2" />
-                </motion.div>
+                    <motion.div
+                        initial="hidden"
+                        animate="visible"
+                        variants={imageOneVariants}
+                        transition={{ duration: 0.7, delay: 0 }}
+                        className={styles.imageOneWrapper}>
+                        <Image className={styles.imageOne} src={image1} alt="image1" />
+                    </motion.div>
 
-                <motion.div
-                    initial="hidden"
-                    animate="visible"
-                    variants={imageThreeVariants}
-                    transition={{ duration: 0.2, delay: 0.9 }}
-                    className={styles.imageThreeWrapper}>
-                    <Image className={styles.imageThree} src={image3} alt="image3" />
-                </motion.div>
+                    <motion.div
+                        initial="hidden"
+                        animate="visible"
+                        variants={imageThreeVariants}
+                        transition={{ duration: 0.2, delay: 0.9 }}
+                        className={styles.imageThreeWrapper}>
+                        <Image className={styles.imageThree} src={image3} alt="image3" />
+                    </motion.div>
 
-                <motion.div
-                    initial="hidden"
-                    animate="visible"
-                    variants={imageThreeVariants}
-                    transition={{ duration: 0.2, delay: 0.9 }}
-                    className={styles.imageThreeAltWrapper}>
-                    <Image className={styles.imageThreeAlt} src={image3} alt="image3alt" />
-                </motion.div>
+                    <motion.div
+                        initial="hidden"
+                        animate="visible"
+                        variants={imageThreeVariants}
+                        transition={{ duration: 0.2, delay: 0.9 }}
+                        className={styles.imageThreeAltWrapper}>
+                        <Image className={styles.imageThreeAlt} src={image3} alt="image3alt" />
+                    </motion.div>
 
-                <motion.div
-                    initial="hidden"
-                    animate="visible"
-                    variants={imageFourVariants}
-                    transition={{ duration: 0.7, delay: 1.2 }}
-                    className={styles.imageFourWrapper}>
-                    <Image className={styles.imageFour} src={image4} alt="image4" />
-                </motion.div>
-            </div>
+                    <motion.div
+                        initial="hidden"
+                        animate="visible"
+                        variants={imageFourVariants}
+                        transition={{ duration: 0.7, delay: 1.2 }}
+                        className={styles.imageFourWrapper}>
+                        <Image className={styles.imageFour} src={image4} alt="image4" />
+                    </motion.div>
+                </div>
 
-            <div className={styles.socialMediaContainer}>
-                <div className={styles.socialMediaInnerDiv}>
+                <div className={styles.socialMediaContainer}>
                     <motion.div
                         initial={{ y: 200, opacity: 0 }}
                         animate={{ y: 0, opacity: 1 }}
-                        transition={{ duration: 0.5, ease: "circOut", delay: 1.2 }}>
-                        <Facebook strokeWidth={0.75} />
+                        transition={{ duration: 0.5, ease: "circOut", delay: 1.7 }}>
+                        <div className={styles.contactSection}>
+                            Have questions? Email us at
+                            <a href="mailto:hello@nexrentertainment.com" className={styles.emailLink}>
+                                hello@nexrentertainment.com
+                            </a>
+                        </div>
                     </motion.div>
-                    <motion.div
-                        initial={{ y: 200, opacity: 0 }}
-                        animate={{ y: 0, opacity: 1 }}
-                        transition={{ duration: 0.5, ease: "circOut", delay: 1.4 }}>
-                        <Instagram strokeWidth={0.75} />
-                    </motion.div>
-                    <motion.div
-                        initial={{ y: 200, opacity: 0 }}
-                        animate={{ y: 0, opacity: 1 }}
-                        transition={{ duration: 0.5, ease: "circOut", delay: 1.6 }}>
-                        <Linkedin strokeWidth={0.75} />
-                    </motion.div>
+                    <div className={styles.socialMediaInnerDiv}>
+                        <motion.div
+                            initial={{ y: 200, opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            transition={{ duration: 0.5, ease: "circOut", delay: 1.2 }}>
+                            <Facebook strokeWidth={0.75} />
+                        </motion.div>
+                        <motion.div
+                            initial={{ y: 200, opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            transition={{ duration: 0.5, ease: "circOut", delay: 1.4 }}>
+                            <Instagram strokeWidth={0.75} />
+                        </motion.div>
+                        <motion.div
+                            initial={{ y: 200, opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            transition={{ duration: 0.5, ease: "circOut", delay: 1.6 }}>
+                            <Linkedin strokeWidth={0.75} />
+                        </motion.div>
+                    </div>
                 </div>
             </div>
         </div>
